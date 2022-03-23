5fb34507c72c2c82fbc8a1f116a238d9a51b4816
aschmitigal22 <aschmitigal22@mylcusd.net>
2022-03-23 15:09:12 -0700

    New Theme, Better Favicon, & PWA

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const cacheName = 'lchs-go';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/settings.js',
+        '/scheduleFormatting.js',
+        '/vueScript.js'
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- vueScript.js
+++ vueScript.js
@@ -60,33 +60,57 @@ PetiteVue.createApp({
   settingsMenu,
   settings,
   changedSetting: true,
+  shareLink: "",
 
   // Functions
   switchPage(page) {
-    if (page == 'now' || page == 'calendar' || page == 'settings')
+    if (page == 'now' || page == 'calendar' || page == 'settings' || page == 'data')
       this.currentPage = page;
     else {
       this.currentPage = 'now';
     }
 
     window.history.pushState("", this.currentPage, "/?" + this.currentPage);
-    // location.search = currentPage;
+    this.popupDate = "";
+    this.shareLink = "";
   },
   changeSetting,
   changeHue,
+  themeChange,
   changeClassName,
   translateWithInsert, 
   translate,
   getMonthText,
   mod,
+  shareSettings,
+  shareClassNames,
 
   // Update interval timer
   interval: null,
   startTimer() {
     var url = new URLSearchParams(location.search);
-    this.switchPage(url.keys().next().value);
-    this.update();
+    var startReload = false;
+    if (url.has("setSettings")) {
+      localStorage.setItem("settings", url.get("setSettings"));
+      startReload = true;
+    }
+    if (url.has("setClassNames")) {
+      localStorage.setItem("customNamesJSON", url.get("setClassNames"));
+      startReload = true;
+    }
+    if (url.has("calendar")) {
+      this.switchPage("calendar");
+    } else if (url.has("settings")) {
+      this.switchPage("settings");
+    } else if (url.has("data")) {
+      this.switchPage("data");
+    } else {
+      this.switchPage("now");
+    }
+    if (startReload) location.reload();
+  
     changeHue(settings.colorTheme);
+    this.update();
 
     clearInterval(this.interval);
     this.interval = setInterval(() => {
@@ -116,8 +140,8 @@ PetiteVue.createApp({
 
     this.todaysGreeting = getTodaysGreeting();
     this.minutesLeft = this.currentPeriod.end.diff(dayjs(), "minutes") + 1;
-    this.percentCompleted = 100 - (this.minutesLeft / this.currentPeriod.end.diff(this.currentPeriod.start, "minutes")) * 100;
-    this.percentCompletedText = translateWithInsert( "PERCENT_COMPLETED", Math.trunc(this.percentCompleted));
+    this.percentCompleted = Math.trunc(100 - (this.minutesLeft / this.currentPeriod.end.diff(this.currentPeriod.start, "minutes")) * 100);
+    this.percentCompletedText = translateWithInsert( "PERCENT_COMPLETED", this.percentCompleted);
     this.currentTime = dayjs().format(timeFormat);
     document.title = (this.minutesLeft >= 60 ? (Math.trunc(this.minutesLeft / 60) + "hr. ") : "") + this.minutesLeft % 60 + "min. | LCHS Go";
   },
@@ -257,17 +281,50 @@ function changeHue(hue) {
     value = hslToHex(0, 0, 90);
   } else if (hue == 360) {
     value = hslToHex(0, 0, 25);
+  } else if (hue == 359) {
+    value = hslToHex(0, 0, 0);
   }
   document.getElementById("background").style.backgroundColor = value;
 
-  var iconImage
-  if (settings.colorTheme == 0)
-    iconImage = "/favicons/favicon0.ico"
-  else if (settings.colorTheme == 360)
-    iconImage = "/favicons/favicon18.ico"
-  else
-    iconImage = ("/favicons/favicon" + Math.floor(settings.colorTheme / 360 * 17 + 1) + ".ico");
-  changeIcon(iconImage);
+  var canvas = document.createElement('canvas');
+  canvas.width = 48;canvas.height = 48;
+  var ctx = canvas.getContext('2d');
+  var img = new Image();
+  img.src = '/faviconClear.png';
+  img.onload = function() {
+      ctx.fillStyle = value;
+      ctx.beginPath();
+      ctx.arc(24, 24, 24, 0, 2 * Math.PI);
+      ctx.fill();
+      if (hue == 0) {
+        ctx.filter = 'invert(1)';
+      }
+      ctx.drawImage(img, 0, 0);
+
+      var link = document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      link.href = canvas.toDataURL("image/x-icon");
+      document.getElementsByTagName('head')[0].appendChild(link);
+  }
+
+  // var iconImage
+  // if (settings.colorTheme == 0)
+  //   iconImage = "/favicons/favicon0.ico"
+  // else if (settings.colorTheme >= 359)
+  //   iconImage = "/favicons/favicon18.ico"
+  // else
+  //   iconImage = ("/favicons/favicon" + Math.floor(settings.colorTheme / 360 * 17 + 1) + ".ico");
+  // changeIcon(iconImage);
+}
+
+var prevTime = Date.now();
+function themeChange() {
+  if (Date.now() - prevTime < 100) {
+    document.getElementById("background").style.backgroundColor = "";
+    document.getElementById("background").style.backgroundImage = "linear-gradient(0,rgba(255, 0, 0, 1) 0%,rgba(255, 154, 0, 1) 10%,rgba(208, 222, 33, 1) 20%,rgba(79, 220, 74, 1) 30%,rgba(63, 218, 216, 1) 40%,rgba(47, 201, 226, 1) 50%,rgba(28, 127, 238, 1) 60%,rgba(95, 21, 242, 1) 70%,rgba(186, 12, 248, 1) 80%,rgba(251, 7, 217, 1) 90%,rgba(255, 0, 0, 1) 100%)"
+  }
+  prevTime = Date.now()
 }
 
 // Function - Helper for ^ to change HSL to Hex
@@ -305,6 +362,20 @@ function changeClassName(periodId, element) {
     customNames[periodId] = newValue;
   }
   localStorage.setItem("customNamesJSON", JSON.stringify(customNames));
+}
+
+function shareSettings() {
+  var link = new URL(location.origin);
+  console.log(settings);
+  link.searchParams.set("setSettings", JSON.stringify(settings));
+  this.shareLink = link.href + "&settings";
+}
+
+function shareClassNames() {
+  var link = new URL(location.origin);
+  console.log(settings);
+  link.searchParams.set("setClassNames", JSON.stringify(customNames));
+  this.shareLink = link.href + "&settings";
 }
 
 // Function - Used to translate a key to the selected language.
