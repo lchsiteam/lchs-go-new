43e466777d160ef1611255ed008c31d0faa0f730
aschmitigal22 <aschmitigal22@gmail.com>
2023-08-25 03:04:06 -0700

    Rename and consolidate

---
+++ src/index.js
@@ -0,0 +1,458 @@
+// Importing the Schedule, Settings, and Languages
+import { formattedJSON, languageJSON, scheduleJSON, getSchedule, getEvent } from "./schedule.js";
+import { settings, settingsMenu } from "./settings.js";
+
+// Stores the user preference for how they display time
+const timeOffeset = dayjs.tz(scheduleJSON.timeOffset, "HH:mm:ss", scheduleJSON.timezone).local();
+let timeFormat = (settings.twentyFourHour ? "HH" : "h") + ":mm" + (settings.showAMPM ? " A" : "");
+
+const rootStyle = document.querySelector(":root").style;
+
+// Export the current settings JSON
+let customNames = JSON.parse(localStorage.getItem("customNamesJSON"));
+if (!customNames) {
+  customNames = {};
+  window.postMessage({ namesChanged: true }); // Send a message for the extension to pick up on when the settings change
+  localStorage.setItem("customNamesJSON", JSON.stringify(customNames));
+}
+
+// Find current Period
+let currentPeriod = null;
+let periodListComponent = PeriodListComponent(formattedJSON, false);
+
+// Stores if a notification has been sent already
+let notified = false;
+
+periodListComponent.listPeriod.forEach((p) => {
+  if (p.isCurrent()) {
+    currentPeriod = p;
+  }
+});
+
+export const Pages = {
+  Now: "now",
+  Calendar: "calendar",
+  CalendarDay: "calendarDay",
+  Settings: "settings",
+  ClassNames: "classNames",
+  Data: "data",
+  values: () => Object.values(Pages).filter((v) => typeof v === "string"),
+  contains: (page) => Pages.values().includes(page),
+  filter: (filter) => Pages.values().filter(filter),
+  map: (map) => Pages.values().map(map),
+};
+
+// Petite Vue interface
+// Anything in here is accessible in the HTML
+PetiteVue.createApp({
+  // Components
+  PeriodInformationComponent,
+  PeriodListComponent,
+  CalendarDay,
+
+  // All Pages
+  Pages,
+  currentPage: Pages.Now,
+  backgroundColor: "hsl( 0, 50, 50)",
+
+  // Now Page
+  periodListComponent,
+  todaysGreeting: "",
+  listCount: 0,
+  getListCount() {
+    return this.listCount++, this.listCount % 2 == 0;
+  },
+  currentPeriod,
+  currentTime: 0,
+  minutesLeft: 0,
+  percentCompleted: 0,
+  percentCompletedText: "",
+
+  // Calendar Page
+  showPopup: false,
+  // popupSchedule: periodList,
+  selectedCalendarDay: null,
+  monthOffset: 0,
+  calendarToggle: true,
+
+  // Settings Page
+  settingsMenu,
+  settings,
+  changedSetting: true,
+  shareLink: "",
+
+  // Class Names Page
+  customNames,
+
+  // Functions
+  switchPage(page) {
+    this.currentPage = Pages.contains(page) ? page : Pages.Now;
+    this.popupDate = null;
+
+    window.history.pushState("", this.currentPage, "/?" + this.currentPage);
+
+    // If on smaller window scroll down to the period details box
+    if (window.innerWidth <= 767 && page === Pages.Now) {
+      let el = document.getElementsByClassName("period-details-small")[0];
+      if (el != null) el.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  },
+  changeSetting,
+  changeHue,
+  themeChange,
+  changeClassName,
+  translateWithInsert,
+  translate,
+  translateNoCustom,
+  getMonthText,
+  mod,
+  shareSettings,
+  shareClassNames,
+  getEvent,
+
+  // Update interval timer
+  interval: null,
+  startTimer() {
+    const url = new URLSearchParams(location.search);
+    let startReload = false;
+    if (url.has("setSettings")) localStorage.setItem("settings", url.get("setSettings")), (startReload = true);
+    if (url.has("setClassNames")) localStorage.setItem("customNamesJSON", url.get("setClassNames")), (startReload = true);
+
+    const page = Pages.filter((p) => url.has(p))[0];
+    if (page) this.switchPage(page);
+    else this.switchPage(Pages.Now);
+
+    if (startReload) location.reload();
+
+    changeHue(settings.colorTheme);
+    this.update();
+
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.update();
+    }, 5000);
+  },
+  update() {
+    if (!this.currentPeriod.isCurrent()) {
+      if (this.currentPeriod.start.day() != dayjs().day()) location.reload();
+
+      periodListComponent.listPeriod.forEach((p) => {
+        if (p.isCurrent()) {
+          this.currentPeriod = p;
+        }
+      });
+      notified = false;
+    }
+
+    this.todaysGreeting = getTodaysGreeting();
+    this.minutesLeft = this.currentPeriod.end.diff(dayjs().subtract(timeOffeset.hour(), "hour").subtract(timeOffeset.minute(), "minute").subtract(timeOffeset.second(), "second"), "minutes") + 1;
+    this.percentCompleted = Math.trunc(100 - (this.minutesLeft / this.currentPeriod.end.diff(this.currentPeriod.start, "minutes")) * 100);
+    this.percentCompletedText = translateWithInsert("PERCENT_COMPLETED", this.percentCompleted);
+    this.currentTime = dayjs().format(timeFormat);
+    document.title = (this.minutesLeft >= 60 ? Math.trunc(this.minutesLeft / 60) + "hr. " : "") + (this.minutesLeft % 60) + "min. | LCHS Go";
+    sendNotification(this.currentPeriod, this.minutesLeft);
+  },
+}).mount();
+
+// Component - Period - Holds the name, start, end, and if passing
+function PeriodComponent(setName, setStart, setEnd, setPassing) {
+  const varStart = dayjs(setStart, "hh:mm A"); //formats from the json
+  const varEnd = dayjs(setEnd, "hh:mm A");
+
+  return {
+    name: setName,
+    start: varStart,
+    end: varEnd,
+    passing: setPassing,
+    getStart() {
+      return varStart.format(timeFormat);
+    },
+    getEnd() {
+      return varEnd.format(timeFormat);
+    },
+    isCurrent() {
+      const now = dayjs().subtract(timeOffeset.hour(), "hour").subtract(timeOffeset.minute(), "minute").subtract(timeOffeset.second(), "second");
+      return now.isBetween(this.start, this.end);
+    },
+    isVisible() {
+      if (this.isCurrent() || settings.showExtraPeriods) return true;
+      else {
+        if (this.name == "PERIOD_0" && !settings.zeroEnabled) return false;
+        else if (this.name == "PERIOD_6" && !settings.sixthEnabled) return false;
+        else if (setPassing) return false;
+        else return true;
+      }
+    },
+    getName() {
+      if (this.passing) {
+        let tempName = this.name.split(",");
+        return translateWithInsert(tempName[0], translate(tempName[1]));
+      }
+      return translate(this.name);
+    },
+  };
+}
+
+// Component - CalendarDay - Holds the schedule for the day and the date
+function CalendarDay(day, monthOffset) {
+  var dateS = dayjs()
+    .month(dayjs().month() + monthOffset)
+    .startOf("month");
+
+  var dateM = dateS.set("date", day - dateS.day());
+  return {
+    date: dateM,
+    schedule: getSchedule(dateM),
+    event: getEvent(dateM),
+  };
+}
+
+// Component - Period Information Template - Used to make a period information block
+function PeriodInformationComponent(props) {
+  return {
+    $template: "#period-information-template",
+  };
+}
+
+// Component - Period List Template - Used to make a period list block
+function PeriodListComponent(periods, isCal) {
+  if (periods == null && isCal) return {};
+  var periodList = periods
+    .map((p) => {
+      return PeriodComponent(p.name, p.start, p.end, p.passing);
+    })
+    .filter((p) => p.isVisible() || (p.isCurrent() && !isCalendar));
+  return {
+    isCalendar: isCal,
+    listPeriod: periodList,
+    $template: "#period-list-template",
+  };
+}
+
+// Function - Get the translated greeting and schedule for the day
+export function getTodaysGreeting() {
+  return getGreeting() + " " + translateWithInsert("TODAY_IS", translate(formattedJSON.scheduleType));
+}
+
+// Function - Get the translated current month for the calendar
+export function getMonthText(month) {
+  var monthsDict = {
+    0: "JANUARY",
+    1: "FEBURARY",
+    2: "MARCH",
+    3: "APRIL",
+    4: "MAY",
+    5: "JUNE",
+    6: "JULY",
+    7: "AUGUST",
+    8: "SEPTEMBER",
+    9: "OCTOBER",
+    10: "NOVEMBER",
+    11: "DECEMBER",
+  };
+  return translate(monthsDict[dayjs().month(month).month()]);
+}
+
+// Function - Get the time of day for the greeting
+function getGreeting() {
+  var hours = dayjs().hour();
+
+  if (hours < 12) return translate("MORNING");
+  else if (hours < 18) return translate("AFTERNOON");
+  else return translate("EVENING");
+}
+
+// Function - Set the local storage settings with an updated user setting
+function changeSetting(setting, value) {
+  settings[setting] = value;
+  this.changedSetting = !this.changedSetting;
+
+  // Send a message for the extension to pick up on when the settings change
+  window.postMessage({ settingsChanged: true });
+
+  localStorage.setItem("settings", JSON.stringify(settings));
+
+  timeFormat = (settings.twentyFourHour ? "HH" : "h") + ":mm" + (settings.showAMPM ? " A" : "");
+
+  if (setting == "themeAnimationIntensity") rootStyle.setProperty("--animated-background-intensity", value + "deg");
+
+  if (setting == "notificationToggle" && value) {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert(translate("NOTIFY_UNSUPPORTED"));
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("LCHS Go", { body: translate("NOTIFY_ENABLED"), icon: "/faviconLarge.png" });
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("LCHS Go", { body: translate("NOTIFY_ENABLED"), icon: "/faviconLarge.png" });
+          // …
+        }
+      });
+    }
+  }
+}
+
+// Function - Called by the HTML to set the background color
+function changeHue(hue) {
+  // Set the background color to the hue, with 50% saturation and 50% lightness
+  var hslValue = [hue, 50, 50];
+  rootStyle.setProperty("--text-color", "white");
+
+  // Overrides for certain colors
+  // Black text on white background
+  if (hue == 0) {
+    rootStyle.setProperty("--text-color", "black");
+    hslValue = [0, 0, 90];
+  }
+  // Solid dark gray background for the last hue
+  else if (hue == 360) hslValue = [0, 0, 25];
+  // Solid black background for one off the last hue
+  else if (hue == 359) hslValue = [0, 0, 0];
+
+  const hslString = `hsl(${hslValue[0]} ${hslValue[1]}% ${hslValue[2]}%)`;
+  rootStyle.setProperty("--background-color", hslString);
+
+  /*
+  This code is used to change the favicon color
+  It works by drawing a colored circle on a canvas
+  Then it draws the favicon on top of the circle
+  Then it sets the favicon to the canvas using a data URL
+  */
+  var canvas = document.createElement("canvas");
+  canvas.width = 48;
+  canvas.height = 48;
+  var ctx = canvas.getContext("2d");
+  var img = new Image();
+  img.src = "/public/faviconClear.png";
+  img.onload = function () {
+    ctx.fillStyle = hslString;
+    ctx.beginPath();
+    ctx.arc(24, 24, 24, 0, 2 * Math.PI);
+    ctx.fill();
+    if (hue == 0) ctx.filter = "invert(1)";
+
+    ctx.drawImage(img, 0, 0);
+
+    var link = document.createElement("link");
+    link.type = "image/x-icon";
+    link.rel = "shortcut icon";
+    link.href = canvas.toDataURL("image/x-icon");
+    document.getElementsByTagName("head")[0].appendChild(link);
+  };
+}
+
+/**
+ * Function - Change the theme of the page based on the time of day
+ */
+var prevTime = Date.now();
+function themeChange() {
+  if (Date.now() - prevTime < 100) {
+    rootStyle.setProperty("--background-color", "transparent");
+    document.getElementById("background").style.backgroundImage = "linear-gradient(0,rgba(255, 0, 0, 1) 0%,rgba(255, 154, 0, 1) 10%,rgba(208, 222, 33, 1) 20%,rgba(79, 220, 74, 1) 30%,rgba(63, 218, 216, 1) 40%,rgba(47, 201, 226, 1) 50%,rgba(28, 127, 238, 1) 60%,rgba(95, 21, 242, 1) 70%,rgba(186, 12, 248, 1) 80%,rgba(251, 7, 217, 1) 90%,rgba(255, 0, 0, 1) 100%)";
+  }
+  prevTime = Date.now();
+}
+
+// Function - Update the local storage for the customNames to the new custom names
+function changeClassName(periodId, element) {
+  var newValue = element.value;
+  if (newValue == languageJSON[periodId] || newValue == "") {
+    newValue = languageJSON[periodId];
+    delete customNames[periodId];
+    element.value = newValue;
+  } else {
+    customNames[periodId] = newValue;
+  }
+  // Send a message to the extension to update custom names
+  window.postMessage({ namesChanged: true });
+
+  localStorage.setItem("customNamesJSON", JSON.stringify(customNames));
+}
+
+function shareSettings() {
+  var link = new URL(location.origin);
+  link.searchParams.set("setSettings", JSON.stringify(settings));
+  this.shareLink = link.href + "&settings";
+}
+
+function shareClassNames() {
+  var link = new URL(location.origin);
+  link.searchParams.set("setClassNames", JSON.stringify(customNames));
+  this.shareLink = link.href + "&settings";
+}
+
+/**
+ * Function - Used to translate a key to the selected language.
+ * @param {string} translateText - The key to translate.
+ * @returns {string} - The translated text.
+ */
+export function translate(translateText) {
+  if (customNames[translateText] != null) {
+    return customNames[translateText];
+  } else {
+    return translateNoCustom(translateText);
+  }
+}
+
+/**
+ * Used to translate a key to the selected language without custom names.
+ * @param {string} translateText - The key to translate.
+ * @returns {string} - The translated text.
+ */
+export function translateNoCustom(translateText) {
+  if (languageJSON[translateText] == null) {
+    return translateText;
+  } else {
+    return languageJSON[translateText];
+  }
+}
+
+/**
+ * Function - Used to translate a key to the selected language and insert other text if possible.
+ * @param {string} translateText - The key to translate.
+ * @param {string} insertString - The string to insert if possible.
+ * @returns {string} - The translated text.
+ */
+export function translateWithInsert(translateText, insertString) {
+  var returnText = translate(translateText);
+  var index = returnText.indexOf("{}");
+  if (index < 0) return translate(translateText);
+  return returnText.slice(0, index) + insertString + returnText.slice(index + 2);
+}
+
+// Notify the user before period starts or ends
+export function sendNotification(period, timeLeft) {
+  if (settings.notificationToggle && !notified) {
+    var nextPeriod = null;
+    periodListComponent.listPeriod.forEach((p) => {
+      if (p.getStart() == period.getEnd()) {
+        nextPeriod = p;
+      }
+    });
+    if (nextPeriod && !nextPeriod.passing && nextPeriod.isVisible() && timeLeft == parseInt(settings.notificationStart)) {
+      // period start notif
+      const notification = new Notification("LCHS Go", { body: nextPeriod.getName() + translateWithInsert("NOTIFY_START", translate(settings.notificationStart)), icon: "/faviconLarge.png" });
+      notified = true;
+    } else if (!period.passing && period.isVisible() && timeLeft == parseInt(settings.notificationEnd)) {
+      // period end notif
+      const notification = new Notification("LCHS Go", { body: period.getName() + translateWithInsert("NOTIFY_END", translate(settings.notificationEnd)), icon: "/faviconLarge.png" });
+      notified = true;
+    }
+  }
+}
+
+export function mod(bigNum, smallNum) {
+  var output;
+  if (bigNum < 0) {
+    output = smallNum - (-bigNum % smallNum);
+  } else {
+    output = bigNum % smallNum;
+  }
+  return output;
+}
---
+++ src/schedule.js
@@ -0,0 +1,192 @@
+// Import the user settings
+import { settings } from "./settings.js";
+
+// Export the JSON parsed in this file
+export var scheduleJSON = JSON.parse(localStorage.getItem("scheduleJSON"));
+export var languageJSON = JSON.parse(localStorage.getItem("languageJSON"));
+export var eventsJSON = JSON.parse(localStorage.getItem("eventsJSON"));
+
+var reload = false;
+async function checkDataVersion(response, existingData) {
+  const data = await response.json();
+  if (existingData == null || existingData.version < data.version) return (reload = true), data;
+  return Promise.reject();
+}
+
+Promise.allSettled([
+  // Fetch the schedule.json for updates
+  fetch("/data/schedule.json")
+    .then((r) => checkDataVersion(r, scheduleJSON))
+    .then((serverScheduleJSON) => {
+      localStorage.setItem("scheduleJSON", JSON.stringify(serverScheduleJSON));
+      scheduleJSON = serverScheduleJSON;
+    }),
+
+  // Fetch the language.json for update
+  fetch("/data/languages.json")
+    .then((r) => checkDataVersion(r, languageJSON))
+    .then((serverLanguageJSON) => {
+      var tempJSON = serverLanguageJSON[settings.language];
+      tempJSON.version = serverLanguageJSON.version;
+      tempJSON.language = settings.language;
+      localStorage.setItem("languageJSON", JSON.stringify(tempJSON));
+    }),
+
+  // Fetch the events.json for updates
+  fetch("/data/events.json")
+    .then((r) => checkDataVersion(r, eventsJSON))
+    .then((serverEventsJSON) => {
+      localStorage.setItem("eventsJSON", JSON.stringify(serverEventsJSON));
+      eventsJSON = serverEventsJSON;
+    }),
+]).then(() => {
+  if (reload) location.reload();
+});
+
+// Create and export the formattedJSON for today
+export var formattedJSON = getSchedule(dayjs());
+
+// Set timezone for dayjs (not sure if it does anything)
+dayjs.tz.setDefault(scheduleJSON.timezone);
+
+/**
+ * Get the formatted schedule json for a specific day
+ * @param {dayjs()} date - The date to get the schedule for
+ * @returns {JSON} - The formatted schedule json
+ */
+export function getSchedule(date) {
+  if (date == null) return;
+  var scheduleType;
+  var localJSON = [];
+
+  // Check if an override exists for the date
+  if (Object.keys(scheduleJSON.overrides.all).includes(date.format("MM/DD/YYYY"))) {
+    scheduleType = scheduleJSON.overrides.all[date.format("MM/DD/YYYY")];
+  } else if (Object.keys(scheduleJSON.overrides[settings.grade]).includes(date.format("MM/DD/YYYY"))) {
+    scheduleType = scheduleJSON.overrides[settings.grade][date.format("MM/DD/YYYY")];
+  } else {
+    // Check if today is in a range
+    var isBreak = inBreak(date);
+    var isCustomWeek = inCustomWeek(date);
+    if (isBreak) {
+      scheduleType = isBreak;
+    } else if (isCustomWeek) {
+      scheduleType = scheduleJSON.customWeeks[isCustomWeek][date.day()];
+    } else {
+      scheduleType = scheduleJSON.defaults[date.day()];
+    }
+  }
+
+  // Add the periods and passing periods the json
+  if (scheduleType != "NONE" && !Object.keys(scheduleJSON.dateRanges.breaks).includes(scheduleType)) {
+    var previousEnd;
+    switch (settings.grade) {
+      case "GRADE_7":
+      case "GRADE_8":
+        Object.keys(scheduleJSON.gradeLevels.middleSchool[scheduleType]).forEach((period) => {
+          if (previousEnd != undefined) {
+            localJSON.push({
+              name: "PASSING_BEFORE," + period,
+              start: previousEnd,
+              end: scheduleJSON.gradeLevels.middleSchool[scheduleType][period][0],
+              passing: true,
+            });
+          }
+          localJSON.push({
+            name: period,
+            start: scheduleJSON.gradeLevels.middleSchool[scheduleType][period][0],
+            end: scheduleJSON.gradeLevels.middleSchool[scheduleType][period][1],
+            passing: false,
+          });
+          previousEnd = scheduleJSON.gradeLevels.middleSchool[scheduleType][period][1];
+        });
+        break;
+      case "GRADE_9":
+      case "GRADE_10":
+      case "GRADE_11":
+      case "GRADE_12":
+        Object.keys(scheduleJSON.gradeLevels.highSchool[scheduleType]).forEach((period) => {
+          if (previousEnd != undefined) {
+            localJSON.push({
+              name: "PASSING_BEFORE," + period,
+              start: previousEnd,
+              end: scheduleJSON.gradeLevels.highSchool[scheduleType][period][0],
+              passing: true,
+            });
+          }
+          localJSON.push({
+            name: period,
+            start: scheduleJSON.gradeLevels.highSchool[scheduleType][period][0],
+            end: scheduleJSON.gradeLevels.highSchool[scheduleType][period][1],
+            passing: false,
+          });
+          previousEnd = scheduleJSON.gradeLevels.highSchool[scheduleType][period][1];
+        });
+        break;
+    }
+    if (settings.grade >= 9) {
+    }
+
+    // Add before and after school
+    localJSON = [
+      {
+        name: "BEFORE_SCHOOL",
+        start: dayjs().startOf("day"),
+        end: localJSON[0].start,
+        passing: true,
+      },
+      ...localJSON,
+      {
+        name: "AFTER_SCHOOL",
+        start: localJSON[localJSON.length - 1].end,
+        end: dayjs().endOf("day"),
+        passing: true,
+      },
+    ];
+  } else {
+    // Add only no school
+    localJSON = [
+      {
+        name: "NONE",
+        start: dayjs().startOf("day"),
+        end: dayjs().endOf("day"),
+        passing: false,
+      },
+    ];
+  }
+
+  // Add the scheduleType to the json
+  localJSON.scheduleType = scheduleType;
+  return localJSON;
+}
+
+// Function - Check if a date is in a break from the schedule.json and get that break if so
+function inBreak(date) {
+  var breakType = false;
+  for (var range in scheduleJSON.dateRanges.breaks) {
+    if (date.startOf().add(1, "hour").isBetween(dayjs(scheduleJSON.dateRanges.breaks[range][0], "MM/DD/YYYY").startOf("day"), dayjs(scheduleJSON.dateRanges.breaks[range][1], "MM/DD/YYYY").endOf("day"))) {
+      breakType = range;
+      break;
+    }
+  }
+  return breakType;
+}
+
+// Function - Check if a date is in a custom week from the schedule.json and get that week if so
+function inCustomWeek(date) {
+  var weekType = false;
+  for (var range in scheduleJSON.dateRanges.customWeeks) {
+    if (date.startOf().add(1, "hour").isBetween(dayjs(scheduleJSON.dateRanges.customWeeks[range][0], "MM/DD/YYYY").startOf("day"), dayjs(scheduleJSON.dateRanges.customWeeks[range][1], "MM/DD/YYYY").endOf("day"))) {
+      weekType = range;
+      break;
+    }
+  }
+  return weekType;
+}
+
+export function getEvent(date) {
+  if (date.year() in eventsJSON) {
+    return eventsJSON[date.year()][dayjs.months()[date.month()]][date.date()];
+  }
+  return undefined;
+}
