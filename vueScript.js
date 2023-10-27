6a49ad2a0ab3fc761db7a30c6df4761fc8f4f4c7
Apricitine <apricitinecas@gmail.com>
2023-10-27 09:18:05 -0700

    please i really should not be committing to prod

--- vueScript.js
+++ vueScript.js
@@ -26,7 +26,7 @@ periodListComponent.listPeriod.forEach((p) => {
 console.log("Hello, things are broken.");
 localStorage.removeItem("isExtension");
 localStorage.removeItem("changelog");
-if("numberOfClicks" in localStorage.settings) localStorage.removeItem("settings");
+if(localStorage.settings.hasOwnProperty("numberOfClicks")) localStorage.removeItem("settings");
 
 // Petite Vue interface
 PetiteVue.createApp({
